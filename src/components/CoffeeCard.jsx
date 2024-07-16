407723d7e528606680c1097f740708334fcd87ba
sowmik-sec <sowmik.sec@gmail.com>
2024-07-16 15:42:06 +0600

    show coffees

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+function CoffeeCard({ coffee, coffees, setCoffees }) {
+  const { _id, name, quantity, supplier, taste, photo } = coffee;
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={photo} alt="Movie" />
+      </figure>
+      <div className="flex justify-between w-full pr-4">
+        <div>
+          <h2 className="card-title">Name: {name}</h2>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="btn-group btn-group-vertical space-y-4">
+            <button className="btn">View</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn">Edit</button>
+            </Link>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-orange-500"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CoffeeCard;
