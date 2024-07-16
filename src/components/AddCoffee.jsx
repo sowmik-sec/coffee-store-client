e2ac9452f63c081afbdd3eca7a00c02b090c5eb3
sowmik-sec <sowmik.sec@gmail.com>
2024-07-16 15:26:30 +0600

    send coffee to backend

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 function AddCoffee() {
   const handleAddCoffee = (event) => {
     event.preventDefault();
@@ -23,6 +25,25 @@ function AddCoffee() {
     };
 
     console.log(newCoffee);
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Coffee Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
   };
   return (
     <div className="bg-[#F4F3F0] p-24">
